f759ebb97fd8a9b5b00b2a3d935275cc6506b951
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-04-21 18:34:08 +0200

    Changed logic to welcome calls from various sources

--- app/lib/generateImage.jsx
+++ app/lib/generateImage.jsx
@@ -2,13 +2,22 @@ import satori from "satori";
 import { readFileSync } from "fs";
 import { join } from "path";
 import * as style from "./style_components/styles";
+import { pinOnPinata, calculateCID } from "@/app/lib/utils";
 import sharp from "sharp";
+import { Readable } from "stream";
 
 // Loading fonts
 const gothamBoldItalic = join(process.cwd(), "public/fonts/GothamBoldItalic.ttf");
 const gothamBoldItalicData = readFileSync(gothamBoldItalic);
 
-export async function generateFriendImage(callerUsername, callerPropic, friendUsername, friendPropic) {
+export async function generateFriendImage(
+  callerUsername,
+  callerPropic,
+  friendUsername,
+  friendPropic,
+  onlyCID = false,
+  onlyPin = false
+) {
   // Get current date to show on the image
   const currentDate = new Date();
   const formattedDate = currentDate.toLocaleDateString();
@@ -59,12 +68,45 @@ export async function generateFriendImage(callerUsername, callerPropic, friendUs
 
   console.log("SVG generated successfully");
 
-  const outputPath = join(process.cwd(), "public/frames/test.png");
-  const sharpBuffer = sharp(Buffer.from(svg)).toFormat("png");
+  const sharpPNG = sharp(Buffer.from(svg)).toFormat("png");
+  const buffer = await sharpPNG.toBuffer();
 
   // Save the image to the file system if we are running locally
   if (process.env.NEXT_PUBLIC_BASE_URL && process.env.NEXT_PUBLIC_BASE_URL.includes("localhost")) {
-    await sharpBuffer.toFile(outputPath);
+    const outputPath = join(process.cwd(), "public/frames/test.png");
+    await sharpPNG.toFile(outputPath);
   }
-  return await sharpBuffer.toBuffer();
+
+  const json_test = {
+    description: "Friendly OpenSea Creature that enjoys long swims in the ocean.",
+    external_url: "https://openseacreatures.io/3",
+    image: "https://storage.googleapis.com/opensea-prod.appspot.com/puffs/3.png",
+    name: "Dave Starbelly",
+    attributes: {
+      trait_type: "Base",
+      value: "Starfish",
+    },
+  };
+
+  // If onlyCID is true, we calculate the CID and return it
+  if (onlyCID) {
+    const imageCid = await calculateCID(buffer);
+    console.log("Image CID: ", imageCid);
+
+    const jsonCid = await calculateCID(Buffer.from(JSON.stringify(json_test)));
+    console.log("JSON CID: ", jsonCid);
+  }
+  // If onlyPin is true, we pin the image to Pinata and return the pin
+  else if (onlyPin) {
+    const stream = new Readable();
+    stream.push(buffer);
+    stream.push(null);
+
+    const { jsonHash, imageHash } = await pinOnPinata(json_test, stream);
+    console.log("Pinned image: ", imageHash);
+    console.log("Pinned json: ", jsonHash);
+    return { jsonHash, imageHash };
+  }
+  // Otherwise, we return the buffer
+  return buffer;
 }
