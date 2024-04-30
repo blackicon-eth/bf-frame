1b1efcd620cf8c0ad28790b47ad35a78a35ecbd2
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-04-30 02:32:01 +0200

    Changed function to wait for it to finish

--- app/api/post_transaction/route.ts
+++ app/api/post_transaction/route.ts
@@ -25,15 +25,14 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
 
   const imageBuffer = await generateFriendImage(callerUsername, callerPropic, friendUsername, friendPropic);
 
-  // Call pinOnPinata without waiting for it to end
-  pinOnPinata(imageBuffer, friendUsername, callerUsername, friendshipLevel)
-    .then(({ imageResponse, jsonResponse }) => {
-      console.log("Pinned image: ", imageResponse);
-      console.log("Pinned json: ", jsonResponse);
-    })
-    .catch((error) => {
-      console.error("Error pinning on Pinata: ", error);
-    });
+  // Call pinOnPinata function to pin the image and json on Pinata
+  try {
+    const { imageResponse, jsonResponse } = await pinOnPinata(imageBuffer, friendUsername, callerUsername, friendshipLevel);
+    console.log("Pinned image: ", imageResponse);
+    console.log("Pinned json: ", jsonResponse);
+  } catch (error) {
+    console.error("Error pinning on Pinata: ", error);
+  }
 
   // Creating the frame
   const frame = getFrameHtmlResponse({
